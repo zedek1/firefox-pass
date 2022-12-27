642dec306a3347c436e498071b39b742dd2d3636
zedek <ethan168861@gmail.com>
2022-12-27 10:44:10 +1000

    Fixed headers & moved functions from main

--- src/ffbt.hxx
+++ src/ffbt.hxx
@@ -3,20 +3,26 @@
 #include "nss.hxx"
 #include "crypt.hxx"
 #include <fstream>
+
+#include <ShlObj.h>
+#include <wil/resource.h>
 #include <nlohmann/json.hpp>
 #include <sqlite3.h>
+
+std::filesystem::path get_nss_location();
+std::vector<std::filesystem::path> get_valid_profiles();
 
 class FFBT : public NSS
 {
 public:
     // credentials
-    int find_credentials(std::filesystem::path profile_path);
+    int retrieve_credentials(std::filesystem::path profile_path, int output_type);
 
     // decryption
-    std::vector<std::string> get_sqlite_credentials(std::filesystem::path signons_path);
+    std::vector<std::vector<std::string>> get_sqlite_credentials(std::filesystem::path signons_path);
     std::vector<std::vector<std::string>> get_json_credentials(std::filesystem::path logins_path);
-    void decrypt_credentials(std::vector<std::vector<std::string>> encrypted_creds);
+    std::vector<std::vector<std::string>> decrypt_credentials(std::vector<std::vector<std::string>> encrypted_creds);
 
     // output
-    void output_credentials(); // later
+    int output_credentials(std::vector<std::vector<std::string>> decrypted_creds, int output_type);
 };
